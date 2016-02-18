219d1c7ba38c0a345cd39b3c77aee8691cf0580e
JimmySevere <JimmySevere@users.noreply.github.com>
2016-02-18 20:26:41 +0000

    First commit

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php 
+
+//Reject if accessed directly
+defined( 'ABSPATH' ) || die( 'Our survey says: ... X.' );
+
+//Plugin code
+
+/*add_action( 'admin_footer_text', function(){
+	echo get_site_option( 'PLUGIN_TEMPLATE_version' );
+} );*/
---
+++ updates/0.1.0/update.php
@@ -0,0 +1,45 @@
+<?php 
+
+//Reject if accessed directly
+defined( 'BOP_PLUGIN_ACTIVATING' ) || die( 'Our survey says: ... X.' );
+
+//Update (or install) script
+
+//DB
+global $wpdb;
+
+//Guide: https://codex.wordpress.org/Creating_Tables_with_Plugins
+//Check https://core.trac.wordpress.org/browser/trunk/src/wp-admin/includes/schema.php#L0 for example sql
+
+/*
+$charset_collate = $wpdb->get_charset_collate();
+
+$sql = "CREATE TABLE {$wpdb->PLUGIN_TEMPLATE_TABLE} (
+		meta_id bigint(20) unsigned NOT NULL auto_increment,
+		term_id bigint(20) unsigned NOT NULL default '0',
+		meta_key varchar(255) default NULL,
+		meta_value longtext,
+		PRIMARY KEY  (meta_id),
+		KEY term_id (term_id),
+		KEY meta_key (meta_key($max_index_length))
+	) $charset_collate;";
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+dbDelta( $sql );
+
+unset( $sql, $charset_collate );
+*/
+
+//WP Cron
+
+/*
+$args = array();
+
+if( wp_next_scheduled( 'my_old_hook', $args );
+
+$args = array();
+
+wp_schedule_event( time(), 'hourly', 'my_schedule_hook', $args );
+
+unset( $args );
+*/
